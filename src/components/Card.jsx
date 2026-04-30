7c43364b7e375dfd07008d56c20d64aed4f846aa
Filippo Casadei <casadeifilippo91@gmail.com>
2026-04-30 17:54:15 +0200

    -Creato componente Card

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+function Card () {
+    return (
+        <div className="card mt-3">
+            <div className="card-body">
+                <p className="mb-0">Testo</p>
+            </div>
+        </div>
+    )
+};
+
+export default Card;
