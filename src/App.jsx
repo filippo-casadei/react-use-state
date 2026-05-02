deaad46ea5eee3a4cd9b6a1f6d5661721245e0c8
Filippo Casadei <casadeifilippo91@gmail.com>
2026-05-02 16:32:40 +0200

    -App gestisce lo stato del bottone attivo e Card mostra dinamicamente la descrizione del linguaggio selezionato tramite props

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
 import Card from './components/Card.jsx';
 import Bottone from './components/Bottone.jsx';
 import BottoneList from './components/BottoneList.jsx';
+import languages from './data/languages.js';
 import { useState } from 'react';
 import './App.css'
 
 
 function App() {
   const [attivo, setAttivo] = useState(null);
-  
+
+  let selectedLanguage = undefined;
+
+  if (attivo !== null) {
+    selectedLanguage = languages.find(function (elemento) {
+      return elemento.id === attivo;
+    });
+  }
+
   return (
     <>
-    <BottoneList attivo={attivo} setAttivo={setAttivo} />
-    <Card />
+      <BottoneList attivo={attivo} setAttivo={setAttivo} />
+      <Card selectedLanguage={selectedLanguage} />
     </>
   )
 };
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,13 @@
-function Card () {
+function Card (props) {
+
+    if (props.selectedLanguage === undefined) {
+        return null;
+    }
+
     return (
         <div className="card mt-3">
             <div className="card-body">
-                <p className="mb-0">Testo</p>
+                <p className="mb-0"> {props.selectedLanguage.description} </p>
             </div>
         </div>
     )
