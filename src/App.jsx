c2e0a1b7057755ccf5b0b24838a8194631148bdf
Filippo Casadei <casadeifilippo91@gmail.com>
2026-05-02 15:35:01 +0200

    - Aggiunto stato attivo button.jsx - Aggiunta atato anche ad App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import Card from './components/Card.jsx';
 import Bottone from './components/Bottone.jsx';
 import BottoneList from './components/BottoneList.jsx';
+import { useState } from 'react';
 import './App.css'
 
 
 function App() {
+  const [attivo, setAttivo] = useState(null);
+  
   return (
     <>
-    <BottoneList />
+    <BottoneList attivo={attivo} setAttivo={setAttivo} />
     <Card />
     </>
   )
--- src/components/Bottone.jsx
+++ src/components/Bottone.jsx
@@ -1,6 +1,24 @@
+
 function Bottone (props) {
+
+
+
+    const clickHandler = (click) => {
+        console.log(props.id)
+        props.setAttivo(props.id);
+    };
+
+    let classe = "";
+    if (props.attivo === props.id) {
+        classe = "btn btn-warning mt-2";
+    }
+    else {
+        classe = "btn btn-primary mt-2"
+    }
+
     return (
-            <button className="btn btn-primary mt-2">
+            <button className={classe}
+            onClick={clickHandler}>
             {props.titolo}   
             </button>
     )
