5b93c99185596055e93c5002607de7fc7486bbf4
Filippo Casadei <casadeifilippo91@gmail.com>
2026-05-02 15:39:57 +0200

    -Correzione passaggio props in BottoneLis

--- src/components/BottoneList.jsx
+++ src/components/BottoneList.jsx
@@ -1,12 +1,16 @@
 import languages from "../data/languages.js";
 import Bottone from "./Bottone.jsx";
 
-function BottoneList() {
+function BottoneList(props) {
     return (
         <div className="d-flex gap-2 flex-wrap">
             {languages.map((elemento) => {
                 return (
-                    <Bottone key={elemento.id} id={elemento.id} titolo={elemento.title} />
+                    <Bottone key={elemento.id} 
+                    id={elemento.id} 
+                    titolo={elemento.title}  
+                    attivo={props.attivo}
+                    setAttivo={props.setAttivo} />
                 );
             })}
         </div>
